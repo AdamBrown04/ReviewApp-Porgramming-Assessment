b3b24ae09a9300de5d13e908d3c3cdf09d27ffe8
AdamBrown04 <adambrown2004@outlook.com>
2023-11-16 16:38:52 +0000

    made it so LoginForm loads first

--- ReviewApp-Porgramming-Assessment/Program.cs
+++ ReviewApp-Porgramming-Assessment/Program.cs
@@ -11,7 +11,7 @@ namespace ReviewApp_Porgramming_Assessment
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ReviewForm());
+            Application.Run(new LoginForm());
         }
     }
 }
